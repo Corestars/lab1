be7ea460bce6a07f50d2e9364d61f249a57805b9
Corestars <esfiveseven@aliyun.com>
2017-11-27 16:07:42 +0800

    111111

---
+++ src/lab1/dgsaasdaa.java
@@ -0,0 +1,23 @@
+package lab1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class dgsaasdaa {
+
+	@Test
+	public void testRead() {
+		String customs=readin.read("1");
+		System.out.println(customs);
+		String customss=readin.read("2");
+		System.out.println(customss);
+		String customsss=readin.read("3");
+		System.out.println(customsss);
+		String customssss=readin.read("4");
+		System.out.println(customssss);
+		String customsssss=readin.read("5");
+		System.out.println(customsssss);
+	}
+
+}
